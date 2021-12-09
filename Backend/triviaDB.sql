06e5695f33757e5d0ab7a2b05508280239449941
AlanaCrognale <crognale.alana@gmail.com>
2021-12-08 19:29:03 -0500

    Add files via upload

---
+++ Backend/triviaDB.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS triviaDB;
+CREATE DATABASE triviaDB;
+
+USE triviaDB;
+
+CREATE TABLE player(
+playerName VARCHAR(45) PRIMARY KEY NOT NULL,
+playerPassword VARCHAR(400) NOT NULL);
+
+CREATE TABLE game(
+gameId INT PRIMARY KEY AUTO_INCREMENT,
+playerName VARCHAR(45) NOT NULL,
+category VARCHAR(45) NOT NULL,
+score INT NOT NULL,
+correct INT NULL,
+wrong INT  NULL,
+FOREIGN KEY (playerName) REFERENCES player(playerName));
---
+++ Backend/triviaDBData.sql
@@ -0,0 +1,30 @@
+USE triviaDB;
+
+INSERT INTO player VALUES
+	('kmbappe7', 'kmbappe7'),
+    ('sakurajima.mai', 'sakurajima.mai'),
+    ('azusugawa.sakuta', 'azusugawa.sakuta'),
+    ('armstrong', 'armstrong'),
+    ('lockhart', 'lockhart');
+    
+INSERT INTO game VALUES
+	(1, 'kmbappe7', 'Sports', 15, 10, 3),
+    (2, 'kmbappe7', 'General Knowledge', 30, 15, 0),
+    (3, 'kmbappe7', 'Animals', 30, 15, 1),
+    (4, 'kmbappe7', 'Video Games', 27, 14, 1),
+    (5, 'sakurajima.mai', 'Sports', 18, 11, 3),
+    (6, 'sakurajima.mai', 'General Knowledge', 7, 5, 3),
+    (7, 'sakurajima.mai', 'Animals', 27, 14, 1),
+    (8, 'sakurajima.mai', 'Video Games', 4, 4, 3),
+    (9, 'azusugawa.sakuta', 'Sports', 12, 8, 3),
+    (10, 'azusugawa.sakuta', 'General Knowledge', 26, 13, 2),
+    (11, 'azusugawa.sakuta', 'Animals', 3, 3, 3),
+    (12, 'azusugawa.sakuta', 'Video Games', 5, 5, 3),
+	(13, 'armstrong', 'Sports', 27, 14, 1),
+    (14, 'armstrong', 'General Knowledge', 7, 6, 3),
+    (15, 'armstrong', 'Animals', 24, 13, 2),
+    (16, 'armstrong', 'Video Games', 0, 0, 3),
+	(17, 'lockhart', 'Sports', 12, 7, 3),
+    (18, 'lockhart', 'General Knowledge', 30, 15, 0),
+    (19, 'lockhart', 'Animals', 21, 12, 3),
+    (20, 'lockhart', 'Video Games', 8, 6, 3);
